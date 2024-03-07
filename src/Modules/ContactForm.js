eec060c99f6fc4eb4cccaf78ba6d0e3ab1c43337
kasualkid12 <pdwalker3@gmail.com>
2024-03-07 15:41:31 -0600

    adjusted netlify form

--- src/Modules/ContactForm.js
+++ src/Modules/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ContactForm() {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" netlify>
       <input type="hidden" name="form-name" value="contact" />
       
       <label htmlFor="name">Name:</label>
