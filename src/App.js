59b0260b196e4fee4760da7c75e6b64c3654914a
kasualkid12 <pdwalker3@gmail.com>
2024-03-08 15:33:24 -0600

    added Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import Terminal from './Modules/Terminal';
 import ContactForm from './Modules/ContactForm';
 import './Styles/App.scss';
+import Header from './Modules/Header';
 
 const MainContent = () => (
   <div className="main-content">
     {/* Your main content goes here */}
+    <Header />
     <h1>Main Portfolio Content</h1>
     <ContactForm />
   </div>
---
+++ src/Modules/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import '../Styles/Header.scss'; // Adjust the path as needed
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="nav-links">
+          <a href="#home" className="nav-link">
+            Home
+          </a>
+          <a href="#about" className="nav-link">
+            About Me
+          </a>
+          <a href="#projects" className="nav-link">
+            Projects
+          </a>
+          <a href="#contact" className="nav-link">
+            Contact Me
+          </a>
+        </div>
+
+        <div className="icon-links">
+          <a
+            href="https://www.linkedin.com/in/pauldwalkeriii/"
+            className="icon-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://github.com/kasualkid12"
+            className="icon-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
