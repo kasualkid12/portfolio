563ce056dc72c15132e4d94324a86b090d1baa8c
kasualkid12 <pdwalker3@gmail.com>
2024-03-09 16:03:15 -0600

    added section to contact form

--- src/Modules/ContactForm.js
+++ src/Modules/ContactForm.js
@@ -11,39 +11,39 @@ function ContactForm() {
   const characterLimit = 2000;
 
   return (
-    <div className="contact-form">
-      <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="form-inputs">
-          <div className="input-row">
-            <div className="input-group">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" required />
+      <section id='contact' className="contact-form">
+        <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="form-inputs">
+            <div className="input-row">
+              <div className="input-group">
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+              </div>
+              <div className="input-group">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              </div>
             </div>
             <div className="input-group">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required />
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                name="message"
+                required
+                maxLength={characterLimit}
+                value={message}
+                onChange={handleInputChange}
+                placeholder="Describe your project (max 2000 characters)"
+              ></textarea>
+              <small>
+                {message.length}/{characterLimit}
+              </small>
             </div>
           </div>
-          <div className="input-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              required
-              maxLength={characterLimit}
-              value={message}
-              onChange={handleInputChange}
-              placeholder="Describe your project (max 2000 characters)"
-            ></textarea>
-            <small>
-              {message.length}/{characterLimit}
-            </small>
-          </div>
-        </div>
-        <button type="submit">Send</button>
-      </form>
-    </div>
+          <button type="submit">Send</button>
+        </form>
+    </section>
   );
 }
 
