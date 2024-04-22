0f385363a1429d283c4f2c3ad40dec2e0cb893d0
kasualkid12 <pdwalker3@gmail.com>
2024-04-22 16:24:40 -0500

    added extra padding to contact form and animation

--- src/Modules/ContactForm.js
+++ src/Modules/ContactForm.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Aos from 'aos';
 import '../Styles/ContactForm.scss';
 
 function ContactForm() {
   const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    Aos.init({ duration: 1200 });
+  }, []);
 
   const handleInputChange = (event) => {
     setMessage(event.target.value); // Update state when text changes
@@ -11,8 +16,9 @@ function ContactForm() {
   const characterLimit = 2000;
 
   return (
-      <section id='contact' className="contact-form">
-        <h2 className='title'>Contact me here</h2>
+    <section id="contact" className="contact-form">
+      <div className='form-div' data-aos="fade-up">
+        <h2 className="title">Contact me here</h2>
         <form name="contact" method="POST" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
           <div className="form-inputs">
@@ -44,6 +50,7 @@ function ContactForm() {
           </div>
           <button type="submit">Send</button>
         </form>
+      </div>
     </section>
   );
 }
